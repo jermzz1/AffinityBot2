8e683b7b33ff5c4c8461c564c96bec6d52ae05e7
jermzz1 <31583486+jermzz1@users.noreply.github.com>
2022-01-30 15:07:34 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 require('dotenv').config() // Load .env file
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
 const axios = require('axios')
 const Discord = require('discord.js')
 const client = new Discord.Client()
-const PORT = process.env.PORT || 5000
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 function getPrices() {
 
